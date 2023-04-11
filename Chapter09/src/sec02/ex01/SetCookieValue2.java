018bf370200bb8e7d4e908013d1b6cbd6e1450e3
NIGHTPURI <127381985+NIGHTPURI@users.noreply.github.com>
2023-04-11 13:08:45 +0900

    Add files via upload

---
+++ Chapter09/src/sec02/ex01/SetCookieValue2.java
@@ -0,0 +1,44 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SetCookieValue
+ */
+@WebServlet("/set2")
+public class SetCookieValue2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub	      
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		
+		Date d = new Date();
+		
+		// Cookie 객체를 생성한 후 cookieTest 이름으로 한글 정보를 인코딩하여 쿠키에 저장.
+		Cookie c = new Cookie("cookieTest", URLEncoder.encode("JSP Programming.","UTF-8"));
+		
+		// 유효기간 설정 24시간
+		c.setMaxAge(-1);
+		
+		// 생성된 쿠키를 브라우저로 전송.
+		response.addCookie(c);
+		out.println("현재시간 : " + d);
+		out.println("<br> 문자열을 Cookie에 저장합니다.");
+	}
+
+}
